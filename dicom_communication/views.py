ed27a8f49a972925f4b5c399507632133ad827b3
Shivani282004 <shi282004@gmail.com>
2024-09-12 14:38:26 +0530

    added newline

--- dicom_communication/views.py
+++ dicom_communication/views.py
@@ -6,3 +6,5 @@ def retrieve_dicom_file(request, dicom_id):
     dicom_file = get_object_or_404(DICOMFile, id=dicom_id)
     response = FileResponse(open(dicom_file.file.path, 'rb'))
     return response
+
+
