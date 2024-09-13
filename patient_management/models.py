b93a0955c761a61467ffc0182c33c3c3328150a8
Sujal Jain V <145462014+sujalFlash@users.noreply.github.com>
2024-09-13 15:58:17 +0530

    Update models.py
    added disease history and disease model

--- patient_management/models.py
+++ patient_management/models.py
@@ -1,3 +1,68 @@
+# patient/models.py (in the Patient Management app)
+import fs
 from django.db import models
+from hospital_management.models import Hospital
 
-# Create your models here.
+class GenderChoices(models.TextChoices):
+    MALE = 'M', 'Male'
+    FEMALE = 'F', 'Female'
+    OTHER = 'O', 'Other'
+
+class Disease(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    contact_number = models.CharField(max_length=15)
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices, default=GenderChoices.MALE)
+    disease_detected = models.BooleanField(default=False)
+    diseases = models.ManyToManyField(Disease, through='DiseaseHistory')
+
+    def __str__(self):
+        return self.name
+
+    def get_disease_status(self):
+        return "Yes" if self.disease_detected else "No"
+import os
+from django.conf import settings
+class DiseaseHistory(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('healed', 'Healed'),
+    ]
+
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='disease_history')
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE, related_name='disease_history')
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, related_name='disease_history')  # Link to Hospital
+    date_diagnosed = models.DateField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')  # New field for disease status
+
+    def __str__(self):
+        return f"{self.disease.name} (Patient: {self.patient.name}, Hospital: {self.hospital.name})"
+
+
+    def get_image_path(self):
+            # Returns the absolute path of the image file
+        return os.path.join(settings.MEDIA_ROOT, self.image_file.name)
+
+
+class Image(models.Model):
+    disease_history = models.ForeignKey('DiseaseHistory', on_delete=models.CASCADE, related_name='images')
+
+    # Store the path of the image file
+    image_path = models.CharField(max_length=255,default=None)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Example of how you might handle file saving manually
+        if hasattr(self, 'image_file') and self.image_file:
+            # Save file to the custom location
+            file_name = os.path.basename(self.image_file.name)
+            self.image_path = fs.save(file_name, self.image_file)
+        super().save(*args, **kwargs)
+    def __str__(self):
+        return f"{self.disease_history}"
