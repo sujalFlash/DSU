726e303a9b38c162aa9ef26dee434a88da54517e
Sujal Jain V <1si22ci052@gmail.com>
2024-09-27 08:43:46 +0530

    added lung_cancer detection app

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="your_package_name",  # Replace with your package name
-    version="0.1.0",  # Replace with your version number
-    description="A short description of your package",
-    packages=find_packages(),  # Automatically find packages
-)
