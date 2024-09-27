07cd8475a31f6dd8ebe3db0b4ec9a09259c4434a
Sujal Jain V <1si22ci052@gmail.com>
2024-09-27 10:28:30 +0530

    added serializer for rest_api_view for viewing medicines

--- medicine_management/rest_view.py
+++ medicine_management/rest_view.py
@@ -1,16 +1,21 @@
-from rest_framework import permissions
+
+from rest_framework import permissions, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from .models import Medicines
+from .serializers import MedicineSerializer
+
 
 class Medicine_View(APIView):
     permission_classes = [IsAuthenticated]
     model = Medicines
+    serializer_class = MedicineSerializer
     def get(self, request):
-        medicines=Medicines.objects.filter(hospital=request.user)
-        return Response(medicines)
+        medicines=Medicines.objects.filter(hospital=request.user.hospital)
+        serializer = MedicineSerializer(medicines, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 
 
