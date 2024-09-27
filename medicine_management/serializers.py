75bbeae1e9bf951dd443c24aaba66e2f7644caca
Sujal Jain V <145462014+sujalFlash@users.noreply.github.com>
2024-09-27 11:58:39 +0530

    Update serializers.py

--- medicine_management/serializers.py
+++ medicine_management/serializers.py
@@ -3,6 +3,6 @@ from .models import Medicines
 class MedicineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medicines
-        fields = ['id', 'brand_name','hospital','chemical_name','manufacturer_company_name','manufacturing_date','expiry_date']
+        fields = ['id', 'brand_name','hospital','chemical_name','manufacturer_company_name','manufacturing_date','expiry_date','is_expired']
 class MedicineCreateSerializer(serializers.ModelSerializer):
     pass
