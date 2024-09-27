2d3f60a390a7357f3cf806e72990487c317fe78e
Sujal Jain V <145462014+sujalFlash@users.noreply.github.com>
2024-09-27 12:19:10 +0530

    Update urls.py

--- medicine_management/urls.py
+++ medicine_management/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .rest_view import Medicine_View
+from .rest_view import Medicine_List_View_from_other_hospitals
 urlpatterns=[
     path('api/medicine_view/',Medicine_View.as_view(), name='medicine_view'),
+    path('api/medicine_from_nearbyhospital/',Medicine_List_View_from_other_hospitals.as_view(),name='medicine_from_nearbyhospital'),
+
 ]
